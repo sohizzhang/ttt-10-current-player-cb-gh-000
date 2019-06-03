ffec28f071e33956a4dbaf1c270e955fc515fea3
S Yoon <sohizzhang@gmail.com>
2019-06-03 21:35:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,13 @@
-turn_count = ["X", "O"]
-  counter = 0
-  turn_count.each do |count|
-    puts "#{count}"
+def turn_count = ["X", "O"]
+counter = 0
+turn_count.each do |count|
+  puts "#{count}"
+  counter += 1
 end
 
 def current_player
+  if turn_count % 2 == 0
+    print "X"
+  else
+    print "O"
 end
