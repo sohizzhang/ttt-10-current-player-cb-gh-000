9a38f95b50fa8e1216faec8f33b8eaf27a62dcd8
S Yoon <sohizzhang@gmail.com>
2019-06-03 21:58:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |space|
-    if space == "X" || space == "O" 
+    if space == "X" || space == "O"
     counter += 1
   end
 end
